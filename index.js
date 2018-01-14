563bc3a76e0ce0290d18122ea5fe548a5b589668
Jay Bednar <ironviralmedia@gmail.com>
2018-01-14 00:31:39 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+function takeANumber(line, name) {
+  line.push(name);
+  return `Welcome, ${name}. You are number ${line.indexOf(name) + 1} in line.`
+}
+
+function nowServing(line) {
+  if (line[0] === undefined) {
+    return "There is nobody waiting to be served!";
+  } else { 
+    return `Currently serving ${line.shift()}.`;
+  }
+}
+
+function currentLine(line){
+  if (line[0] === undefined) {
+    return "The line is currently empty.";
+  } else {
+    let map1 = line.map(x => ` ${line.indexOf(x) + 1}. ${x}`);
+    return `The line is currently:${map1}`;
+  }
+}
