4142971e34c5ea1f39e1262a7b4c914934148970
Jay Bednar <ironviralmedia@gmail.com>
2018-01-14 00:33:48 +0000

    Done.

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function currentLine(line){
   if (line[0] === undefined) {
     return "The line is currently empty.";
   } else {
-    let map1 = line.map(x => ` ${line.indexOf(x) + 1}. ${x}`);
-    return `The line is currently:${map1}`;
+    let deliLine = line.map(x => ` ${line.indexOf(x) + 1}. ${x}`);
+    return `The line is currently:${deliLine}`;
   }
 }
