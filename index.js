fc9709cae213ad9b125a10ea1c378c1cf52915be
CJ Haviland <cj.haviland@yahoo.com>
2018-01-16 20:41:55 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,39 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name){
   katzDeliLine.push(name);
   
-  return katzDeliLine.length;
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length > 0){
+    var serving = katzDeliLine.shift();
+  
+    return `Currently serving ${serving}.`;
+  }
+  else{
+    return `There is nobody waiting to be served!`;
+  }
+}
+
+function currentLine(katzDeliLine){
+  var lineString = `The line is currently`;
+  var lineLength = katzDeliLine.length;
+  
+  if (lineLength > 0){
+    // Add colon for output
+    lineString += `: `;
+    
+    for (var i = 0; i < lineLength; i++){
+      if (i > 0){
+        lineString += `, `;
+      }
+      
+      lineString += `${i + 1}. ${katzDeliLine[i]}`;
+    }
+  }
+  else{
+    lineString += ` empty.`;
+  }
+  
+  return lineString;
 }
