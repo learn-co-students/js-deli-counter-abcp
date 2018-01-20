12d1338494a45494f70b07cb7063df65f217a464
longwu2011@gmail.com <longwu2011@gmail.com>
2018-01-20 00:33:13 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+function takeANumber(line, name){
+  line.push(name)
+  var num = line.indexOf(name) + 1
+  return "Welcome, " + name + ". You are number " + num + " in line."
+}
+function nowServing(line){
+  if (line.length === 0) {return "There is nobody waiting to be served!"}
+  else {return ("Currently serving " + line[0] + ".")}
+  line.shift()
+}
+
+function currentLine(line){
+  var greetings = "The line is currently: 1. " + line[0]
+  if (line.length === 0) {return "The line is currently empty."}
+  else {for (let i = 2; i < line.length + 1; i++) {greetings = greetings + ", " + i + ". " + line[i-1]}
+  return greetings}
+}
