e3cdeb06591ead2f5a39cb5d98f47594c6887f9a
stanciuionelamelania@gmail.com <stanciuionelamelania@gmail.com>
2018-01-20 16:17:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 var katzDeli = [];
 
 function takeANumber(name){
-      katzDeli.push(name)
-   return `Welcome, ${name}. You are number ${katzDeli.lenght} in line.`
+   this.katzDeli.push(name)
+   return `Welcome, ` + name +`. You are number ${katzDeli.indexOf(name)+1} in line.`
 }
 
 function nowServing(){
   if(katzDeli.length > 0){
-     return katzDeli.shift()}
+     return katzDeli.shift()
+   }
      else {
-       return "nobody to be served"
+       return `There is nobody waiting to be served!`
      }
+
 }
 
-function currentLine(katzDeliLine){
-  if(katzDeliLine.length > 0){
-     x = `The line is currently: `
-     for(var i = 0 ; i < katzDeliLine.length; i++)
-        x += '${i}.${katzDeliLine[i]} '
-    return x }
+function currentLine(katzDeli){
+  if(katzDeli.length > 0){
+     var x = `The line is currently:`
+     for(var i = 0 ; i < katzDeli.length - 1; i++)
+        x += ` `+ (i+1) + `. ` + katzDeli[i] + `,`
+      x += ` `+ (i+1) + `. ` + katzDeli[i]
+    return x
+  }
   else {
-    return "The line is currently empty."}
+    return `The line is currently empty.`}
 
 }
