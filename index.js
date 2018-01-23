1d5f82f4c280026b2e20bd151f8d0db9957e57bb
leahbateman@gmail.com Bateman <leahbateman@gmail.com>
2018-01-23 00:42:54 +0000

    Done.

--- index.js
+++ index.js
@@ -4,3 +4,8 @@ function takeANumber(katzDeliLine, newPersonName){
   katzDeliLine.push(newPersonName)
   return "Welcome, " + newPersonName + ". You are number " + katzDeliLine.length + " in line.";
 }
+
+function nowServing(katzDeliLine) {
+  var currPerson = katzDeliLine.pop();
+  return currPerson;
+}
