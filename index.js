c102518b2ee3b1b59469ade746590727155001d6
Robert H Boscacci <cinemarob1@gmail.com>
2018-03-18 02:50:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 function takeANumber(katzDeliLine, newName){
-  console.log(katzDeliLine);
+  katzDeliLine.push(newName);
+  return('Welcome, ' + newName + '. You are number ' + katzDeliLine.length + ' in line.');
+}
+
+function nowServing(katzDeliLine){
+  if (katzDeliLine.length===0){
+    return ('There is nobody waiting to be served!')
+  }else{
+    var firstPerson = katzDeliLine[0];
+    katzDeliLine.shift();
+    return('Currently serving ' + firstPerson + '.');
+  }
+}
+
+function currentLine(katzDeliLine){
+  if (katzDeliLine.length===0){
+    return ('The line is currently empty.')
+  }else{
+    var numbered = [...katzDeliLine]
+    for (let i=0; i<katzDeliLine.length; i++){
+      var j = i+1;
+      numbered[i] = ' ' + j + '. ' + katzDeliLine[i];
+    }
+    return('The line is currently:' + numbered);
+  }
 }
